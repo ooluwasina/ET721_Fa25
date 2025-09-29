7f8a36891d606e776a435047797eefb3f8a09be1
ooluwasina <oluwaferanmi.oluwasina58@student.qcc.cuny.edu>
2025-09-29 10:56:04 -0400

    test

---
+++ lab7_Data/lab7_function.py
@@ -0,0 +1,14 @@
+"""
+Daniel Oluwasina
+Lab 7, accessing data in a file
+
+"""
+
+def testing():
+    print("Daniel Oluwasina")
+
+def read_data(filename):
+    fileuser = open(filename, 'r')
+
+    for each in fileuser:
+        print(each)
---
+++ lab7_Data/lab7_main.py
@@ -0,0 +1,12 @@
+"""
+Daniel Oluwasina
+Lab 7, accessing data in a file
+
+"""
+
+from lab7_function import *
+
+testing()
+print("\n----- Example 1: Reading file -----")
+
+read_data("phrases.txt")
