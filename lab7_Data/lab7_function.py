f714c5dd2c81217b076beeba0da79ba942e9e156
ooluwasina <oluwaferanmi.oluwasina58@student.qcc.cuny.edu>
2025-10-17 06:45:27 +0000

    lab7

--- lab7_Data/lab7_function.py
+++ lab7_Data/lab7_function.py
@@ -1,13 +1,63 @@
 """
 Daniel Oluwasina
 Lab 7, accessing data in a file
-
+Oct 14, 2025
 """   
 def testing():
     print("Daniel Oluwasina")
-
+#Example 1: read file
 def read_data(filename):
-    fileuser = open(filename, 'r')
+    with open(filename, 'r') as file1:
+        filecontent = file1.read()
+        print(filecontent)
 
-    for each in fileuser:
-        print(each)
+    print(f"Is this closed?{file1.closed}")
+#Example 2: reading specific portion of a file
+def read_up(filename):
+    with open(filename, 'r') as file1:
+        #read first 30 char
+        print(file1.read(30))
+        #read the next 5 char
+        print(file1.read(5))
+#Example 3: readline
+def read_lines(filename):
+    with open(filename, 'r') as file1:
+        print(file1.readline(30))
+        print(file1.readline(5))
+#Example 4: readlines
+def read_all(filename):
+    with open(filename, 'r') as file1:
+        print(file1.readlines())
+#Example 5: loop though readlines
+def read_each(filename):
+    with open(filename, 'r') as file1:
+        filelines = file1.readlines()
+        for eachline in filelines:
+            print(eachline.strip())
+#Example 6: create a new file
+def new_file(filename):
+    with open(filename, 'w') as file:
+        file.write("Python Basics for Data Analiysis")
+        file.write("\nDaniel Oluwasina")
+#Example 7: Append data into an existing file
+from datetime import datetime
+def stamp_date(filename):
+    with open(filename, 'a') as file:
+        file.write(f"\n\n{datetime.now()}")
+#Exercise:
+def email_read(filename, email):
+    count_email = 0
+    with open(filename, 'r') as file1:
+        filelines = file1.readlines()
+        for eachline in filelines:
+            if email in eachline:
+                count_email+=1
+                print(eachline.strip())
+                          
+    return count_email 
+def new_email_file(filename, yahoo, gmail, hotmail):
+    with open(filename, 'w') as file:
+        file.write(f"\nyahoo = {yahoo}")
+        file.write(f"\ngmail = {gmail}")
+        file.write(f"\nhotmail = {hotmail}")
+
--- lab7_Data/lab7_main.py
+++ lab7_Data/lab7_main.py
@@ -1,12 +1,35 @@
 """
 Daniel Oluwasina
 Lab 7, accessing data in a file
-
+Oct 14, 2025
 """
-
 from lab7_function import *
 
 testing()
 print("\n----- Example 1: Reading file -----")
-
 read_data("phrases.txt")
+
+print("\n----- Example 2: Read specific portion of a file -----")
+read_up("phrases.txt")
+
+print("\n----- Example 3: Read specific portion of a file using readline -----")
+read_lines("phrases.txt")
+
+print("\n----- Example 4: Read specific portion of a file using readlines-----")
+read_all("phrases.txt")
+
+print("\n----- Example 5: loop through each line-----")
+read_each("phrases.txt")
+
+print("\n----- Example 6: creating a new file-----")
+new_file("oluwasina.txt")
+
+print("\n----- Example 7: append data into an existing file-----")
+stamp_date("oluwasina.txt")
+
+print("\n----- Exrcise -----")
+count_yahoo = email_read("user_email.txt", "@yahoo")
+count_gmail = email_read("user_email.txt", "@gmail")
+count_hotmail = email_read("user_email.txt", "@hotmail")
+
+new_email_file("reportemail.txt", count_yahoo, count_gmail, count_hotmail)
