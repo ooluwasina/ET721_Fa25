9e3dd1715983461746c08e1b2c65309a511bd532
ooluwasina <oluwaferanmi.oluwasina58@student.qcc.cuny.edu>
2025-10-31 02:32:57 +0000

    Flask App

--- lab12_flask/app.py
+++ lab12_flask/app.py
@@ -4,21 +4,28 @@ Oct 27, 2025
 Lab 12, introduction to Flask
 """
 from flask import Flask
-from flask import render_template, url_for
+from flask import render_template, url_for, redirect
 #app object
 app = Flask(__name__)
 #set routing to loading page
 @app.route('/')
 def index():
-    return render_template("index.html")
+    name = "Daniel Oluwasina"
+    fruits = ["apples", "kiwi", "orange"]
+    checkfruit = 'kiwi'
+    return render_template("index.html", username = name, fruits_list = fruits, checkfruit = checkfruit)
     
 @app.route('/about')
 def about():
-    return '<h1>About us</h1>'
+    return render_template("about.html")
 
 @app.route('/quotes')
 def quotes():
-    return '<h1>Quotes</h1>'
+    return render_template("quotes.html")
+
+@app.route('/login')
+def login():
+    return redirect(url_for('index'))
 
 #set app to run if u run the file directly
 
