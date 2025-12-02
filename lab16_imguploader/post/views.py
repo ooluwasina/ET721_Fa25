23ddde3756db9bbf4c93ab1c681c0e6d4773bec7
ooluwasina <oluwaferanmi.oluwasina58@student.qcc.cuny.edu>
2025-12-02 03:11:15 +0000

    Complete back end

---
+++ lab16_imguploader/post/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+from django.views.generic import ListView, CreateView
+from django.urls import reverse_lazy
+from .models import Post
+from .forms import PostForm
+# Create your views here.
+class HomePageView(ListView):
+    model = Post
+    template_name = "home.html"
+
+class CreatePostView(CreateView):
+    model = Post
+    form_class = PostForm
+    template_name = "post.html"
+    success_url = reverse_lazy("home")
+ 
